a3faaaa327b29f41730a094a5831b34b73b0a25b
patrickfleischmann <patrickfleischmann@users.noreply.github.com>
2023-09-30 10:35:39 +0200

    removed gitignore files

--- .dep/vectors.o.d
+++
@@ -1,6 +0,0 @@
-build/obj/vectors.o: \
- ../../chibios2111/os/common/startup/ARMCMx/compilers/GCC/vectors.S \
- ../../chibios2111/os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h \
- source/board.h
-../../chibios2111/os/common/startup/ARMCMx/devices/STM32F4xx/cmparams.h:
-source/board.h:
